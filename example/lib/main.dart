7f99a76c17cf9d73684d48a5959cd2344fcaa2fc
Gibah Joseph <gibahjoe@gmail.com>
2024-01-16 01:54:41 +0100

    docs: added migration notice to docs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2023-10-01T09:26:15.827402
+// Openapi Generator last run: : 2024-01-16T01:49:21.940229
 import 'package:flutter/material.dart';
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
